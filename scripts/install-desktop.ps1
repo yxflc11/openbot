a7d2f0c5410f188c676805d4f7677102662fa0e9
Wen <yxflc11@gmail.com>
2026-09-13 15:31:48 +0800

    chore(desktop): prepare alpha.8 version metadata

--- scripts/install-desktop.ps1
+++ scripts/install-desktop.ps1
@@ -1,4 +1,4 @@
-param([string]$Version = "0.1.0-alpha.7")
+param([string]$Version = "0.1.0-alpha.8")
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 Add-Type -AssemblyName System.Net.Http
